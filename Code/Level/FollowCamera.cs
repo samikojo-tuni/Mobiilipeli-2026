8ecb025f3354baa3ba5ea2063ebd118218621365
Sami Kojo <sami.kojo@tuni.fi>
2026-02-06 13:46:00 +0200

    Add: FollowCamera

---
+++ Code/Level/FollowCamera.cs
@@ -0,0 +1,38 @@
+using System;
+using Godot;
+
+/// <summary>
+/// This Node follows the target node on the screen.
+/// </summary>
+public partial class FollowCamera : Camera2D
+{
+	[Export] private Node2D _target = null;
+	[Export] private float _speed = 5f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		if (_target == null)
+		{
+			GD.PushWarning("FollowCamera: Target is not set!");
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (_target == null)
+		{
+			// There's no target, no can do!
+			return;
+		}
+
+		float deltaTime = (float)delta;
+
+		// Target's position in the game world in global coordinates.
+		Vector2 targetPosition = _target.GlobalPosition;
+		Vector2 currentPosition = GlobalPosition;
+		Vector2 newPosition = currentPosition.Lerp(targetPosition, _speed * deltaTime);
+		GlobalPosition = newPosition;
+	}
+}
